6fbfaaa1c8041d12f5a8c440c3320510a0a75b45
emvaized <maximtsyba@gmail.com>
2024-09-01 18:09:47 +0200

    Use another user agent for test

--- src/background.js
+++ src/background.js
@@ -6,7 +6,9 @@ chrome.runtime.onMessage.addListener(
             case 'fetchLinkInfo': {
                 getLinkPreview(request.url, {
                     // headers: { "user-agent": "googlebot" },
-                    headers: { "user-agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" },
+                    // headers: { "user-agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" },
+                    'user-agent':
+                        'Slackbot-LinkExpanding 1.0 (+https://api.slack.com/robots)',
                     timeout: 3500
                 }).then((data) => sendResponse(data)).catch(error => sendResponse(undefined));
                 return true;
